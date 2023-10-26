97779b340fff6016576d5c9b412f46eeaaefdccc
vxibh <vxx6hxv@gmail.com>
2023-10-26 23:47:59 +0530

    Basic Front-End Added

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
+import React from "react";
 
-export default function Home() {
+export default async function Page() {
   return (
-    <h1 className='font-bold'>Hello There !</h1>
-  )
+    <div className="w-screen min-h-screen bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-6xl font-semibold">Hello There!</h1></div>
+            <p className="max-w-xl mt-4 text-lg text-black-600 font-semibold">Diagnose Yourself within Moments and get a Treatment Plan</p>
+      </div>
+    </div>
+    </div>
+  );
 }
+ 
