71fb1cb24a53fc9aa0bac2996bfeb6b6dfdcbf27
vxibh <vxx6hxv@gmail.com>
2023-10-27 14:44:47 +0530

    Titles updated

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Page() {
   const isAuth =  !!userId
 
   return (
+    
     <div className='w-screen min-h-screen bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400'>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         <div className='flex flex-col items-center text-center'> 
