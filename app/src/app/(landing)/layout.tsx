854972fff032665c3c75721af8fc0dba3171b2c7
vxibh <vxx6hxv@gmail.com>
2023-11-14 20:49:11 +0530

    Landing Page Improved

--- app/src/app/(landing)/layout.tsx
+++ app/src/app/(landing)/layout.tsx
@@ -7,17 +7,17 @@ const LandingLayout = ({
     children: React.ReactNode;
 }) => {
     return (
-        <main className="h-screen overflow auto" style={{
-            backgroundImage: 'url(/wall.jpg)',
+        <main className="h-full overflow-auto" style={{
+            backgroundImage: 'url(/01.jpg)',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center center',
             width: '100%',
             height: '100%',
           }}>
-                <div className="mx-auto max-w-screen-xl h-full w-full">
-                    {children}
-                </div>
+            <div className='mx-auto max-w-screen h-full w-full '>
+                {children}
+            </div>
         </main> 
     )
 }
---
+++ app/src/components/landing-content.tsx
@@ -0,0 +1,13 @@
+export const LandingContent = () => {
+    return (
+        <div className="px-10 pb-20">
+            <h2 className="text-center text-4xl text-white font-extrabold mb-10 ">
+                Testimonials
+            </h2>
+            <div>
+                
+            </div>
+            
+        </div>
+    )
+}
