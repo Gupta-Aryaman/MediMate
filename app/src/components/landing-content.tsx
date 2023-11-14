6b6d1ea340ad14de511f3569585e9404a63147a0
vxibh <vxx6hxv@gmail.com>
2023-11-14 20:51:18 +0530

    Landing Page Improved

--- app/src/components/landing-content.tsx
+++ app/src/components/landing-content.tsx
@@ -2,10 +2,10 @@ export const LandingContent = () => {
     return (
         <div className="px-10 pb-20">
             <h2 className="text-center text-4xl text-white font-extrabold mb-10 ">
-                Testimonials
+                How It Works
             </h2>
             <div>
-                
+
             </div>
             
         </div>
